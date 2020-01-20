e830ee13240741f67dbdf786a85e3bf24ffe1327
ignatovegors <60047566+ignatovegors@users.noreply.github.com>
2020-01-20 23:39:43 +0300

    Update gcd-euclid-modified.py

--- gcd-euclid-modified.py
+++ gcd-euclid-modified.py
@@ -1,14 +1,14 @@
 def gcd_euclid_modified(a, b):
     assert a >= 0 and b >= 0
     if a == 0 or b == 0:
-        return max(a,b)
+        return max(a, b)
     else:
         return gcd_euclid(b % a, a)
 
 
 def main():
     a, b = map(int, input().split())
-    print(gcd(a, b))
+    print(gcd_euclid_modified(a, b))
 
 
 if __name__ == "__main__":
